41e1b325ab84af8515b572431a3bbe42ca90b0f5
amikalperkins <a.mikalperkins@gmail.com>
2021-11-15 16:58:04 -0800

    update

---
+++ catkin_ws/build/my_pkg/CMakeFiles/publisher_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/amikalperkins/git_workspace/robot-learning2/catkin_ws/devel/lib/my_pkg/publisher_node"
+  "/home/amikalperkins/git_workspace/robot-learning2/catkin_ws/devel/lib/my_pkg/publisher_node.pdb"
+  "CMakeFiles/publisher_node.dir/src/publisher_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/publisher_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
